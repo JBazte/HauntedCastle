453775f76c79bdef48a64395c8c5dd186cc0fba1
Joaquín Bazterrica <joaquin.bazterrica@gmail.com>
2019-04-06 23:07:21 -0300

    UI Added

--- Assets/Scripts/DamageEnemies.cs
+++ Assets/Scripts/DamageEnemies.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class DamageEnemies : MonoBehaviour
 {
     public ParticleSystem destoyParticles;
-
-    void OnDestroy()
-    {
-        Instantiate(destoyParticles, transform.position, Quaternion.identity);
-    }
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Enemy")
         {
+            Instantiate(destoyParticles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
 
         if (other.gameObject.tag == "Test")
         {
+            Instantiate(destoyParticles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public int numHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    private PlayerController thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(thePlayer.health > numHearts)
+        {
+            thePlayer.health = numHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < thePlayer.health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
